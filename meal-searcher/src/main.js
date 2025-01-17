e22634db21982426afe15b0ca05e32d01b634f6a
michaels2533 <michael.stawowski@wsu.edu>
2025-01-16 19:50:41 -0500

    Cleaned up the main.js file

--- meal-searcher/src/main.js
+++ meal-searcher/src/main.js
@@ -1,45 +1,49 @@
 const fetchMealsByltr = async () => {
-    try {
-      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b');
-      
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      
-      const data = await response.json();
-      console.log("Fetched data:", data); 
-      return data; // Return the fetched data
-    } catch (error) {
-      console.error('Error fetching data:', error); 
+  try {
+    const response = await fetch(
+      'https://www.themealdb.com/api/json/v1/1/search.php?f=b'
+    );
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-  };
-  
-  const displayMeals = async () =>{
-    const mealsData = await fetchMealsByltr();
-    const meals = mealsData.meals;
 
-    const mealGrid = document.getElementById('meal-grid');
+    const data = await response.json();
+    console.log('Fetched data:', data);
+    return data; // Return the fetched data
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
 
-    mealGrid.innerHTML = '';
+const displayMeals = async () => {
+  const mealsData = await fetchMealsByltr();
+  const meals = mealsData.meals;
 
-    //Makes a card for each meal based on name and puts it in grid
-    meals.forEach(meal => {
-        const mealCard = document.createElement('div');
-        mealCard.classList.add('meal-card');
+  const mealGrid = document.getElementById('meal-grid');
 
-        mealCard.innerHTML = `<h3>${meal.strMeal}</h3>
+  mealGrid.innerHTML = '';
+
+  //Makes a card for each meal based on name and puts it in grid
+  meals.forEach((meal) => {
+    const mealCard = document.createElement('div');
+    mealCard.classList.add('meal-card');
+
+    mealCard.innerHTML = `<h3>${meal.strMeal}</h3>
         <div><img src="${meal.strMealThumb}" alt="${meal.strMeal}"/></div>`;
 
-        mealGrid.appendChild(mealCard)
-    });
-  };
+    mealGrid.appendChild(mealCard);
+  });
+};
 
+const fetchMealLog = async () => {
+  const meals = await fetchMealsByltr(); // Waits for the data to be returned
+  console.log('Meals data:', meals); // Log the returned data here
+};
+
+const main = () => {
+  // Fills the grid with the fetched data from the API
   displayMeals();
+};
 
-  const fetchMealLog = async () => {
-    const meals = await fetchMealsByltr(); // Waits for the data to be returned
-    console.log("Meals data:", meals); // Log the returned data here
-  };
-  
-  fetchMealLog();
-  
+main();
